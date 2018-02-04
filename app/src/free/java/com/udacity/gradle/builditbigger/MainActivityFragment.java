b8217ff2499e9af1164c400e45965f6e9c8af9a9
Eslam-Saeed <eslamsaeed022@gmail.com>
2018-02-05 01:04:20 +0200

    FIixng minor issues

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -19,7 +22,15 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-
+        AdView mAdView = root.findViewById(R.id.adView);
+        if (!getResources().getBoolean(R.bool.isPaid)) {
+            AdRequest adRequest = new AdRequest.Builder()
+                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                    .build();
+            if (mAdView != null)
+                mAdView.loadAd(adRequest);
+        } else
+            mAdView.setVisibility(View.GONE);
         return root;
     }
 }
